5ec03d5ea454becafd914431ab875e67d0855aa4
Meihui_Xiong <xiongmeihui@gwu.edu>
2019-04-09 23:37:48 +0800

    demo-01 rxjs写的

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,14 @@
-let startTime;
-let endTime;
+import Rx from 'rxjs/Rx';
+
 const button = document.getElementById('hold-me');
 const result = document.getElementById('hold-time');
-button.onmousedown = function() {
-    startTime = new Date();
-}
-button.onmouseup = function() {
-    endTime = new Date();
-    const gap = endTime - startTime;
-    result.innerHTML = gap + '';
-}
+
+const mouseDown = Rx.Observable.fromEvent(button, 'mousedown');
+const mouseUp = Rx.Observable.fromEvent(button, 'mouseup');
+const holdTime = mouseUp.timeStamp().withLatestFrom(mouseDown.timeStamp(), (mouseUpEvent, mouseDownEvent) => {
+    return mouseUpEvent.timeStamp - mouseDownEvent.timeStamp;
+});
+
+holdTime.subscribe(ms => {
+    result.innerText = ms;
+})
