59ca23cd778ba555b422d3cb98ab18ed14c4ed6a
mhxiong <mhxiong@yuansuan.cn>
2019-04-18 09:35:25 +0800

    自定义subject

--- src/index.ts
+++ src/index.ts
@@ -1,28 +1,36 @@
-import { Observable } from 'rxjs'
+import { interval } from 'rxjs'
+import { take } from 'rxjs/operators'
+const numbers = interval(1000)
+const takeFourNumbers = numbers.pipe(take(4))
+const observerA = {
+  next: value => console.log('A next: ' + value),
+  error: error => console.log('A error: ' + error),
+  complete: () => console.log('A complete!'),
+}
 
-const observable = new Observable(subscriber => {
-  subscriber.next(1)
-  subscriber.next(2)
-  subscriber.complete()
-  setTimeout(() => {
-    subscriber.next('hi'); // happens asynchronously
-  }, 1000);
-})
-console.log('start')
-const subscription = observable.subscribe(x => console.log(x))
-subscription.unsubscribe()
-console.log('end')
+const observerB = {
+  next: value => console.log('B next: ' + value),
+  error: error => console.log('B error: ' + error),
+  complete: () => console.log('B complete!'),
+}
 
-// const result = document.getElementById('hold-time')
-
-// const mouseDown = fromEvent(button, 'mousedown')
-// const mouseUp = fromEvent(button, 'mouseup')
-// const holdTime = mouseUp
-//   .timeStamp()
-//   .withLatestFrom(mouseDown.timeStamp(), (mouseUpEvent, mouseDownEvent) => {
-//     return mouseUpEvent.timeStamp - mouseDownEvent.timeStamp
-//   })
-
-// holdTime.subscribe(ms => {
-//   result.innerText = ms
-// })
+const subject = {
+  observers: [],
+  addObserver(observer) {
+    this.observers.push(observer)
+  },
+  next(value) {
+    this.observers.forEach(o => o.next(value))
+  },
+  error(error) {
+    this.observers.forEach(o => o.error(error))
+  },
+  complete() {
+    this.observers.forEach(o => o.complete())
+  },
+}
+subject.addObserver(observerA)
+takeFourNumbers.subscribe(subject)
+setTimeout(() => {
+  subject.addObserver(observerB)
+}, 1000)
