4af89ec0e086ac02fd535d659170915ef220d298
mhxiong <mhxiong@yuansuan.cn>
2019-05-13 18:31:39 +0800

    basic version of observable

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,38 @@
+class Observer {
+  private raw = null
+  private continueEmitValue = true
+
+  constructor(rawObserver) {
+    this.raw = rawObserver
+    this.continueEmitValue = true
+  }
+  public next(val) {
+    if (this.continueEmitValue) {
+      this.raw.next(val)
+    }
+  }
+  public error(err) {
+    if (this.continueEmitValue) {
+      this.raw.error(err)
+      this.unsubscribe()
+    }
+  }
+  public complete() {
+    if (this.continueEmitValue) {
+      this.raw.complete()
+      this.unsubscribe()
+    }
+  }
+  public unsubscribe() {
+    this.continueEmitValue = false
+  }
+}
 function create(fn) {
   return {
     subscribe(observer) {
-      fn(observer)
+      const realObserver = new Observer(observer)
+      fn(realObserver)
+      return realObserver
     },
   }
 }
@@ -9,8 +40,9 @@ const observable = create(subscriber => {
   subscriber.next(1)
   subscriber.next(2)
   subscriber.next(3)
-  subscriber.complete()
-  subscriber.next(4)
+  setTimeout(()=> {
+    subscriber.next(4)
+  }, 4000)
 })
 
 const subscriber_01 = {
@@ -19,4 +51,5 @@ const subscriber_01 = {
   complete: () => console.log('complete'),
 }
 
-observable.subscribe(subscriber_01)
+const subscription = observable.subscribe(subscriber_01)
+subscription.unsubscribe()
