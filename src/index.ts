38d821a5b1a44f9f4309ca0ca12678b551501cc0
mhxiong <mhxiong@yuansuan.cn>
2019-05-13 17:15:38 +0800

    lowest version of observable

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,22 @@
-import { ReplaySubject } from 'rxjs';
-const subject = new ReplaySubject(100, 500 /* windowTime */);
- 
-subject.subscribe({
-  next: (v) => console.log(`observerA: ${v}`)
-});
- 
-let i = 1;
-setInterval(() => subject.next(i++), 200);
- 
-setTimeout(() => {
-  subject.subscribe({
-    next: (v) => console.log(`observerB: ${v}`)
-  });
-}, 1000);
- 
+function create(fn) {
+  return {
+    subscribe(observer) {
+      fn(observer)
+    },
+  }
+}
+const observable = create(subscriber => {
+  subscriber.next(1)
+  subscriber.next(2)
+  subscriber.next(3)
+  subscriber.complete()
+  subscriber.next(4)
+})
+
+const subscriber_01 = {
+  next: val => console.log(val),
+  error: () => console.log('error'),
+  complete: () => console.log('complete'),
+}
+
+observable.subscribe(subscriber_01)
